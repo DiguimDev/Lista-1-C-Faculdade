735cbefd69af5c1ffa2d517e308f87a2743b4680
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-03 15:15:22 -0300

    introdução ao C (Faculdade UFG)

---
+++ Aula1/print01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char code = 'A';// tabela asc numero 65
+
+    printf("Simbolo: %c\n", code);
+
+    return 0;
+}
---
+++ Aula1/print02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int x, y;
+    scanf("%d%d", &x, &y);
+    int xy = x * y;
+    printf("O valor deu %d\n", xy);
+    return 0;
+}
+
